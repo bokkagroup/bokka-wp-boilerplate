900f66918047fb7981c64072468092cf9069a9e3
Charlie Stanard <louis.stanard@gmail.com>
2016-11-07 15:26:34 -0700

    Adding a conditional for gfFilterFunction so it only loads on homeowner resources page

--- wp-content/themes/bokka-wp-theme-child/assets/src/js/helpers/forms.js
+++ wp-content/themes/bokka-wp-theme-child/assets/src/js/helpers/forms.js
@@ -43,16 +43,16 @@ if (productTypeData.length > 0 && requestInfoForm.length > 0) {
     }
 }
 
-
-
 //Removes text from cloned text areas
-gfFilterFunction = "gform.addFilter( 'gform_list_item_pre_add', function ( clone ) {" +
+if ($("body").hasClass('page-homeowner-resources')) {
+    gfFilterFunction = "gform.addFilter( 'gform_list_item_pre_add', function ( clone ) {" +
     "    clone.find('td:eq(0) textarea').val('');" +
     "    return clone;" +
     "} );";
 
-var s = document.createElement("script");
-s.type = "text/javascript";
-s.text = gfFilterFunction;
+    var s = document.createElement("script");
+    s.type = "text/javascript";
+    s.text = gfFilterFunction;
 
-$("body").append(s);
+    $("body").append(s);
+}
