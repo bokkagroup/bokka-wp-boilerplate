1a667cb73516dade41bfc836fc2eed0cb51360e2
Charlie Stanard <charlie@bokkagroup.com>
2017-03-09 13:15:21 -0700

    Allow separate thumbnail images on neighborhood overview page

--- wp-content/themes/bokka-wp-theme-child/hooks/query.php
+++ wp-content/themes/bokka-wp-theme-child/hooks/query.php
@@ -13,10 +13,17 @@ function setFeaturedImage($data)
 
         if (isset($post->ID)) {
             if ($post->post_type === 'communities') {
-                $post->featured_image = wp_get_attachment_image_src(
-                    get_post_thumbnail_id($post->ID),
-                    'thumb-product-listing'
-                )[0];
+                if (isset($post->thumbnail) && ($post->thumbnail)) { // use thumbnail field if available
+                    $post->featured_image = wp_get_attachment_image_src(
+                        $post->thumbnail,
+                        'thumb-product-listing'
+                    )[0];
+                } else { // otherwise use brand window image (featured image)
+                    $post->featured_image = wp_get_attachment_image_src(
+                        get_post_thumbnail_id($post->ID),
+                        'thumb-product-listing'
+                    )[0];
+                }
             } elseif (isset($post->post_type) && ( $post->post_type === 'model' || $post->post_type === 'home' || $post->post_type === 'plans' )) {
                 if (isset($post->elevations) && isset($post->elevations[0])) {
                     $post->featured_image = wp_get_attachment_url(
