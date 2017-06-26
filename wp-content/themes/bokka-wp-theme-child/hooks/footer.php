aad142f4dd98508419958d385a3a1052c6268a2a
codewizard <mikemcguire.dev@gmail.com>
2017-06-26 13:02:26 -0600

    Removing resonate script

--- wp-content/themes/bokka-wp-theme-child/hooks/footer.php
+++ wp-content/themes/bokka-wp-theme-child/hooks/footer.php
@@ -12,14 +12,6 @@ function requireReachLocalTracking()
     <?php
 }
 
-add_action('wp_footer', 'requireResonateTrackingPixel');
-function requireResonateTrackingPixel()
-{
-    ?>
-        <div class="footer-pixel"><script language='JavaScript1.1' src='//pixel.mathtag.com/event/js?mt_id=1094385&mt_adid=176173&v1=&v2=&v3=&s1=&s2=&s3='></script></div>
-    <?php
-}
-
 add_action('wp_footer', 'requireGoogleTagManager');
 function requireGoogleTagManager()
 {
